789d157044586e6984ee94a585faba9aed8b3e40
Tann <tianen.chen@outlook.com>
2018-02-14 17:45:16 -0500

    add

---
+++ src/PhaseOne.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class PhaseOne {
+
+    public static void readFile(String fileName){
+        try {
+            File file = new File(fileName);  //文件句柄
+            BufferedReader bufferedReader;
+            ArrayList<String[]> sublists = new ArrayList<>();
+
+            if(file.exists()){
+                bufferedReader = new BufferedReader(new FileReader(file));
+                int maxTuple = 40*1250;
+
+
+                String[][] runs = new String[maxTuple][7];
+                for(int i=0; i<maxTuple; i++){
+                    String line = bufferedReader.readLine();
+                    runs[i][0]= line.substring(0,8);
+                    runs[i][1] = line.substring(8,18);
+                    runs[i][2] = line.substring(18,28);
+                    runs[i][3] = line.substring(28,31);
+                    runs[i][4] = line.substring(31,34);
+                    runs[i][5] = line.substring(34,43);
+                    runs[i][6] = line.substring(43,100);
+                }
+
+                bufferedReader.close();
+
+                //sort
+                Arrays.sort(runs, new Comparator<String[]>() {
+                    @Override
+                    public int compare(String[] tuple1, String[] tuple2) {
+                        return tuple1[0].compareTo(tuple2[0]);
+                    }
+                });
+
+
+                //write sublist into file
+
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("...."));
+                int linecount = 0;
+                for(int i=0; i<maxTuple; i++){
+                    String
+                }
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/RelationDiff.java
+++ src/RelationDiff.java
@@ -1,6 +1,13 @@
 public class RelationDiff {
 
+    public static Long totalMemorySize;
+
+
     public static void main(String[] args){
+        //total available memory
+        System.out.println("Total Memory:" + Runtime.getRuntime().freeMemory() / (1024*1024) + "MB");
+
+
 
     }
 }
